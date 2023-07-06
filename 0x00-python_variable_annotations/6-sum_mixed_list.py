087c42070bdb6b357612dc30b8c39a0e061153c0
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-06 05:07:46 -0700

    Correction to task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """This file contains a Function sum_mixed_list"""
 from typing import List, Union
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """This Function takes in a list of int or float return their sum of float"""
     return sum(mxd_lst)

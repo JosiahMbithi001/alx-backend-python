949a8dc6f920d4c6fbe6699fafdc4a522a6da347
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-06 05:17:48 -0700

    Task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,8 +2,9 @@
 from typing import Callable
 """This File COntains a function make_multiplier"""
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """This Function multiplies a Flaot and returns a Function"""
-    def multiply(n * multiplier) -> float:
-        
-
+    def multiply(n) -> float:
+        return n * multiplier
+    return multiply

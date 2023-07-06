61895d8883c4437a8f542cebb91016d859b22b17
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-06 05:30:39 -0700

    Added comments to Task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+"""This File contains element_length """
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """This Function receives iterable"""
     return [(i, len(i)) for i in lst]

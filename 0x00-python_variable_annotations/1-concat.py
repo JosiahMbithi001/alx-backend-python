15339e1d7cb6b170d1be8e8b14874987001651df
JosiahMbithi001 <mbithishucks@gmail.com>
2023-07-06 03:42:51 -0700

    Task 1 Concatination

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+"""This File Contains A Concatinating Function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This Function returns a concatenated string"""
+    return "{}{}".format(str1, str2)

c60d4f765836004cf0e26a736993a65627c96a17
JosiahMbithi001 <111077290+JosiahMbithi001@users.noreply.github.com>
2023-07-10 16:44:22 +0000

    Added Return Task in line 8 3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,6 @@ from asyncio import Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Task:
     """This Function Creates a Task"""
     return asyncio.create_task(wait_random(max_delay))
